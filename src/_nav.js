566a8972048f87317664be5563f90fe5fc87179a
PatilChinmay2205 <88974044+PatilChinmay2205@users.noreply.github.com>
2022-10-29 10:22:53 +0530

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -1,305 +1,145 @@
-import React from 'react'
-import CIcon from '@coreui/icons-react'
-import {
-  cilBell,
-  cilCalculator,
-  cilChartPie,
-  cilCursor,
-  cilDescription,
-  cilDrop,
-  cilNotes,
-  cilPencil,
-  cilPuzzle,
-  cilSpeedometer,
-  cilStar,
-} from '@coreui/icons'
-import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
 
-const _nav = [
-  {
-    component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
-  },
-  {
-    component: CNavTitle,
-    name: 'Theme',
-  },
-  {
-    component: CNavItem,
-    name: 'Colors',
-    to: '/theme/colors',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Typography',
-    to: '/theme/typography',
-    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Components',
-  },
-  {
-    component: CNavGroup,
-    name: 'Base',
-    to: '/base',
-    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Accordion',
-        to: '/base/accordion',
-      },
-      {
-        component: CNavItem,
-        name: 'Breadcrumb',
-        to: '/base/breadcrumbs',
-      },
-      {
-        component: CNavItem,
-        name: 'Cards',
-        to: '/base/cards',
-      },
-      {
-        component: CNavItem,
-        name: 'Carousel',
-        to: '/base/carousels',
-      },
-      {
-        component: CNavItem,
-        name: 'Collapse',
-        to: '/base/collapses',
-      },
-      {
-        component: CNavItem,
-        name: 'List group',
-        to: '/base/list-groups',
-      },
-      {
-        component: CNavItem,
-        name: 'Navs & Tabs',
-        to: '/base/navs',
-      },
-      {
-        component: CNavItem,
-        name: 'Pagination',
-        to: '/base/paginations',
-      },
-      {
-        component: CNavItem,
-        name: 'Placeholders',
-        to: '/base/placeholders',
-      },
-      {
-        component: CNavItem,
-        name: 'Popovers',
-        to: '/base/popovers',
-      },
-      {
-        component: CNavItem,
-        name: 'Progress',
-        to: '/base/progress',
-      },
-      {
-        component: CNavItem,
-        name: 'Spinners',
-        to: '/base/spinners',
-      },
-      {
-        component: CNavItem,
-        name: 'Tables',
-        to: '/base/tables',
-      },
-      {
-        component: CNavItem,
-        name: 'Tooltips',
-        to: '/base/tooltips',
-      },
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'Buttons',
-    to: '/buttons',
-    icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Buttons',
-        to: '/buttons/buttons',
-      },
-      {
-        component: CNavItem,
-        name: 'Buttons groups',
-        to: '/buttons/button-groups',
-      },
-      {
-        component: CNavItem,
-        name: 'Dropdowns',
-        to: '/buttons/dropdowns',
-      },
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'Forms',
-    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Form Control',
-        to: '/forms/form-control',
-      },
-      {
-        component: CNavItem,
-        name: 'Select',
-        to: '/forms/select',
-      },
-      {
-        component: CNavItem,
-        name: 'Checks & Radios',
-        to: '/forms/checks-radios',
-      },
-      {
-        component: CNavItem,
-        name: 'Range',
-        to: '/forms/range',
-      },
-      {
-        component: CNavItem,
-        name: 'Input Group',
-        to: '/forms/input-group',
-      },
-      {
-        component: CNavItem,
-        name: 'Floating Labels',
-        to: '/forms/floating-labels',
-      },
-      {
-        component: CNavItem,
-        name: 'Layout',
-        to: '/forms/layout',
-      },
-      {
-        component: CNavItem,
-        name: 'Validation',
-        to: '/forms/validation',
-      },
-    ],
-  },
-  {
-    component: CNavItem,
-    name: 'Charts',
-    to: '/charts',
-    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavGroup,
-    name: 'Icons',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'CoreUI Free',
-        to: '/icons/coreui-icons',
-        badge: {
-          color: 'success',
-          text: 'NEW',
-        },
-      },
-      {
-        component: CNavItem,
-        name: 'CoreUI Flags',
-        to: '/icons/flags',
-      },
-      {
-        component: CNavItem,
-        name: 'CoreUI Brands',
-        to: '/icons/brands',
-      },
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'Notifications',
-    icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Alerts',
-        to: '/notifications/alerts',
-      },
-      {
-        component: CNavItem,
-        name: 'Badges',
-        to: '/notifications/badges',
-      },
-      {
-        component: CNavItem,
-        name: 'Modal',
-        to: '/notifications/modals',
-      },
-      {
-        component: CNavItem,
-        name: 'Toasts',
-        to: '/notifications/toasts',
-      },
-    ],
-  },
-  {
-    component: CNavItem,
-    name: 'Widgets',
-    to: '/widgets',
-    icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
-  },
-  {
-    component: CNavTitle,
-    name: 'Extras',
-  },
-  {
-    component: CNavGroup,
-    name: 'Pages',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Login',
-        to: '/login',
-      },
-      {
-        component: CNavItem,
-        name: 'Register',
-        to: '/register',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 404',
-        to: '/404',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 500',
-        to: '/500',
-      },
-    ],
-  },
-  {
-    component: CNavItem,
-    name: 'Docs',
-    href: 'https://coreui.io/react/docs/templates/installation/',
-    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
-  },
-]
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
 
-export default _nav
+// 1.Declare an empty array
+// const arr = [];
+
+// 2.Declare an array with more than 5 number of elements
+// console.log((arra = [1, 2, 3, 4, 5]));
+
+// 3.Find the length of your array
+// console.log(webTechs.length);
+
+// 4.Get the first item, the middle item and the last item of the array
+// console.log(webTechs[0]);
+// console.log(webTechs[(webTechs.length / 2) | 0]);
+// console.log(webTechs[webTechs.length - 1]);
+
+// 5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+// const mixedDataTypes = [
+//   [
+//     "Albania",
+//     "Bolivia",
+//     "Canada",
+//     "Denmark",
+//     "Ethiopia",
+//     "Finland",
+//     "Germany",
+//     "Hungary",
+//     "Ireland",
+//     "Japan",
+//     "Kenya",
+//   ],
+//   ["HTML", "CSS", "JavaScript", "React", "Redux", "Node", "MongoDB"],
+//   4,
+//   5,
+//   6,
+//   "Character",
+//   "Number",
+// ];
+// console.log(mixedDataTypes);
+// console.log(mixedDataTypes.length);
+
+// 6.Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+
+// 7.Print the array using console.log()
+// console.log(itCompanies);
+
+// 8.Print the number of companies in the array
+// console.log(itCompanies[itCompanies.length]);
+
+// 9.Print the first company, middle and last company
+// console.log(itCompanies[0]);
+// console.log(itCompanies[(itCompanies.length / 2) | 0]);
+// console.log(itCompanies[itCompanies.length - 1]);
+
+// 10.Print out each company
+// console.log(itCompanies);
+
+// 11.Change each company name to uppercase one by one and print them out
+// const upper = [
+// itCompanies[0].toUpperCase(),
+// itCompanies[1].toUpperCase(),
+// itCompanies[2].toUpperCase(),
+// itCompanies[3].toUpperCase(),
+// itCompanies[4].toUpperCase(),
+// itCompanies[5].toUpperCase(),
+// itCompanies[6].toUpperCase(),
+// ];
+// console.log(upper.toString());
+
+// 12.Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// console.log(itCompanies.toString());
+
+// 13.Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// let exist = itCompanies.indexOf("IBM");
+// if (exist != -1) {
+// console.log("IBM");
+// } else {
+// console.log("Company Not found");
+// }
+
+// 14.Filter out companies which have more than one 'o' without the filter method
+
+// 15.Sort the array using sort() method
+// console.log(itCompanies.sort());
+
+// 16.Reverse the array using reverse() method
+// console.log(itCompanies.reverse());
+
+// 17.Slice out the first 3 companies from the array
+// console.log(itCompanies.slice(3));
+
+// 18.Slice out the last 3 companies from the array
+// console.log(itCompanies.slice(-3));
+
+// 19.Slice out the middle IT company or companies from the array
+// console.log(itCompanies.splice(2, 2));
+
+// 20.Remove the first IT company from the array
+// console.log(itCompanies.shift());
+
+// 21.Remove the middle IT company or companies from the array
+
+// 22.Remove the last IT company from the array
+// console.log(itCompanies.pop());
+
+// 23.Remove all IT companies
+// while (itCompanies.length > 0) {
+//   itCompanies.pop();
+// }
+// console.log(itCompanies);
+// OR
+// console.log(0, itCompanies.length);
+// OR
+// console.log((itCompanies.length = 0));
